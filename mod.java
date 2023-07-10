efe9e238aae3c1671b9ad35817fe858c9c7a8d57
Amartya Pattanayak <apattanayakofficial@gmail.com>
2023-07-11 02:48:30 +0530

    Intial

---
+++ mod.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class mod {
+
+    public static void main(String[] args) {
+        int a = 59;
+        int b = 236;
+
+        int reminder = b-a;
+        
+       
+       System.out.println(reminder);
+    }
+    
+}
